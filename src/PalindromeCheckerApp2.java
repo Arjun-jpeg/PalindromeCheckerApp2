07e1566a5abd7ac6d26e81d355ebc8ced4fe3742
Arjun <aa6709@srmist.edu.in>
2026-02-25 15:25:40 +0530

    Hi

--- src/PalindromeCheckerApp2.java
+++ src/PalindromeCheckerApp2.java
@@ -6,10 +6,34 @@ class uc1 {
         System.out.println("System initialized successfully");
     }
 }
+class Uc2{
+    String name;
+    public boolean find(){
+        boolean palin=true;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the name of the palindrome you want to check: ");
+        name=input.nextLine();
+        for(int i=0;i<name.length()/2;i++){
+            if(name.charAt(i)!=name.charAt(name.length()-i-1)) {
+                palin=false;
+                break;
+            }
+        }
+        if(palin){
+            System.out.println("It is a Palindrome? " +name);
+        }
+        else{
+            System.out.println("Not a Palindrome " +name);
+        }
+        return palin;
+    }
+}
 public class PalindromeCheckerApp2{
     public static void main(String[] args){
       uc1 ob1=new uc1();
+      Uc2 ob2=new Uc2();
       ob1.show();
+      System.out.println("Status: " +ob2.find());
     }
 }
 

b54428b26bedacbf294025fe9ad58a390423d3f6
Arjun <aa6709@srmist.edu.in>
2026-02-25 14:23:48 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp2.java
+++ src/PalindromeCheckerApp2.java
@@ -1,2 +1,14 @@
-public class PalindromeCheckerApp2 {
+public class uc1 {
+    public void show() {
+        printf("Welcome to palindrome checker app");
+        printf("Version 1.0");
+        printf("System initialized successfully");
+    }
 }
+public class PalindromeCheckerApp2{
+    public static void main(String[] args){
+      uc1 ob1=new uc1();
+      ob1.show();
+    }
+}
+

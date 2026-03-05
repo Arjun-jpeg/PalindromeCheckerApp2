564194c525e3ab97d986cdd3397bd8749375e69a
Arjun <aa6709@srmist.edu.in>
2026-03-05 06:39:18 +0530

    UC6 implementation

--- src/PalindromeCheckerApp2.java
+++ src/PalindromeCheckerApp2.java
@@ -32,20 +32,35 @@ class Uc2{
     }
 }
 
-class Uc3{
+class Uc3 {
+
     String beforerev;
-    String afterrev="";
+
     public void reverse(){
-        Scanner input=new Scanner(System.in);
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Enter a palindrome string to reverse it and verify: ");
+<<<<<<< Updated upstream
         beforerev=input.nextLine();
         for(int i=beforerev.length()-1;i>=0;i--){
             afterrev=afterrev+beforerev.charAt(i);
         }
         if(afterrev.equals(beforerev)){
+=======
+        beforerev = input.nextLine();
+
+        StringBuilder afterrev = new StringBuilder();
+
+        for(int i = beforerev.length() - 1; i >= 0; i--){
+            afterrev.append(beforerev.charAt(i));
+        }
+
+        if(afterrev.toString().equals(beforerev)){
+>>>>>>> Stashed changes
             System.out.println("Palindrome verified");
-            System.out.println("Before: " +beforerev);
-            System.out.println("After reverse: "+afterrev);
+            System.out.println("Before: " + beforerev);
+            System.out.println("After reverse: " + afterrev);
         }
         else{
             System.out.println("Palindrome not verified, before cannot be equal to after");
